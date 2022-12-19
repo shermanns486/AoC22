5badbe8519b0e8059e4c3217d02ce30ec6e2204a
Stefan Hermanns <s.hermanns@web.de>
2022-12-19 19:50:55 +0100

    Base.java: Einlesen der Eingabedatei in eigene Methode ausgelagert, um einfacher Testdaten einzulesen

--- src/main/java/de/shermanns/aoc22/Base.java
+++ src/main/java/de/shermanns/aoc22/Base.java
@@ -1,6 +1,8 @@
 package de.shermanns.aoc22;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import de.shermanns.aoc22.util.Util;
@@ -8,15 +10,17 @@ import de.shermanns.aoc22.util.Util;
 public abstract class Base {
     protected final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-    protected final List<String> zeilen;
+    protected final List<String> zeilen = new ArrayList<>();
 
-    public Base() {
-        super();
+    protected Base() {
+        loadInputFile(getInputFile());
+    }
 
-        final String inputFile = getInputFile();
-        this.logger.info("Lade Datei "
-                         + inputFile);
-        this.zeilen = Util.loadResource(this.getClass(), inputFile, this.logger);
+    public void loadInputFile(final String inputFile) {
+        this.logger.log(Level.INFO, "Lade Datei {0}", inputFile);
+
+        this.zeilen.clear();
+        this.zeilen.addAll(Util.loadResource(this.getClass(), inputFile, this.logger));
     }
 
     protected abstract String getInputFile();
